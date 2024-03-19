dac268a913a2d25c1f6ca79282447093c11f1b90
Will McCudden <willmccudden@gmail.com>
2024-03-20 06:16:36 +1100

    change CORS URL for backend

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const projectLogController = require("./controllers/projectLogController");
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "https://willwebsite.onrender.com"
 }));
 
 connectToDb();
